42565be9f3c68d97256ab31dabd7e23b6a2f5ec8
Trace <trwyne03@gmail.com>
2021-05-31 16:33:22 -0700

    Fixed warning

--- DungeonSource/DungeonCharacter.java
+++ DungeonSource/DungeonCharacter.java
@@ -33,7 +33,7 @@
  * @version 1.0
  */
 
-public abstract class DungeonCharacter implements Comparable
+public abstract class DungeonCharacter implements Comparable<Object>
 {
 	
 	public CharacterValues CharacterValues;
