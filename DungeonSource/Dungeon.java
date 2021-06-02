a508cf31f4ec9184680a0093fd3025f4329abc67
avanchu1 <84860294+avanchu1@users.noreply.github.com>
2021-06-02 12:50:41 -0700

    Add Refactors list.

--- DungeonSource/Dungeon.java
+++ DungeonSource/Dungeon.java
@@ -6,33 +6,78 @@ import dungeonCharacters.FactoryProducer;
 
 /* Team 11 -- Trace Wynecoop, Hieu Tran, Erick Vargas, Arthur Wallace, Alex Vanchu	
  * CSCD212
- * Description: Driver file for Heroes and Monsters project
+ * Description: Driver file for Heroes and Monsters project.
+ * 					
+ *		Refactor Prject:
+ *			History:
+ *				-- Created Abstract Factory class. 
+ *				-- Created Factory Producer class.
+ *				-- Created Hero Factory class.
+ *				-- Created Monster Factory class.
+ *				-- Refactored names:
+ *						-- Heroes:
+ *							-- Rogue class previously Theif class.						Side Note: All Heroes
+ *							-- Paladin class previously Sorcessor class					 and Monsters classes 
+ *							-- Hunter class previously Warrior class.					 have been modified to
+ *  					-- Monsters:													 fit current theme 
+ *							-- BloodOrc class previously Ogre class.					 structure.
+ *							-- DecayingWolf class previously Germlin class
+ *							-- Dragon class previously Skelton class.
+ *				
+ *				-- hitPoints Variable refactored to Health.
+ *				-- blowPoints variable refactored to crushingHitChance.
+ *				-- keyboard file refactored: Replacing two deprecated methods for type casting
+ *				-- CharacterValues class created: Updating old storage handling for data previously held in dataCharacter
+ *				-- Refactored Dungeon file to fix warning created on implementing Comparable <object>
+ *				-- Refactored Dungeon file methods:
+ *                 chooseHero() - Created a loop to validate user input for the hero selection. 
+ *				   Any values not within 1-3 will prompt the user to re-enter the value.
+ *                 generateMonster() - Removed case 3 where Dragon object is created. 
+ *                 If the random value is not 1 or 2, then it would be 3, which is defaulted to the Dragon object creation.
+ *              -- Refactored switch statements.
+ *              -- Refactored Paladin class, ensuring limited healing; there by ending exploit of unlimited healing ensuring no death.
+ *              -- Refactored visablity of all variables to Private in CharacterValues(). (Using setters and getters)
+ *              -- Refactored DungeonCharacter file to ensure getters are used for retrieving character values.
+ *              -- Refactored Monster file for same; ensuring getters and used. 
+ *              -- Refactored Paladin class setting all variables to private; using getters for variables.
+ *              -- Packages created for organization:
+ *              		-- (default package): contains Dungeon class
+ *              		-- dungeonCharacters: contains AbstractFactory, CharacterValues, DungeonCharacter, FactoryProducer, 
+ *              		   					  HeroFactory, and MonsterFactory classes.
+ *               		-- Heroes: contains Rogue, Paladin, Hunter, and Hero classes.
+ *               		-- Monsters: contains BloodOrc, Dragon, DecayingWolf, and Monster classes.
+ *               		-- Keyboard: contains Keyboard class.
+ *              -- Refactored Dungeon class; Created the hero object inside the do...while loop instead of outside.  
+ *				-- Changed return value of the chooseHero method since we have a factory to produce the hero object.
+ *				-- Changed the return value for generateMonster method since e have a factory that can produce a random monster object.
+ *				
+ *		
+ *		Original Dungeon Source:
+ *			 Copyright:    Copyright (c) 2001
+ * 				Company: Code Dogs Inc.
+ * 				I.M. Knurdy
  *
- * Copyright:    Copyright (c) 2001
- * Company: Code Dogs Inc.
- * I.M. Knurdy
- *
- * History:
- *  11/4/2001: Wrote program
- *    --created DungeonCharacter class
- *    --created Hero class
- *    --created Monster class
- *    --had Hero battle Monster
- *    --fixed attack quirks (dead monster can no longer attack)
- *    --made Hero and Monster abstract
- *    --created Warrior
- *    --created Ogre
- *    --made Warrior and Ogre battle
- *    --added battleChoices to Hero
- *    --added special skill to Warrior
- *    --made Warrior and Ogre battle
- *    --created Sorceress
- *    --created Thief
- *    --created Skeleton
- *    --created Gremlin
- *    --added game play features to Dungeon.java (this file)
- *  11/27/2001: Finished documenting program
- * version 1.0
+ * 				History:
+ *  			11/4/2001: Wrote program
+ *    				--created DungeonCharacter class
+ *    				--created Hero class
+ *    				--created Monster class
+ *    				--had Hero battle Monster
+ *    				--fixed attack quirks (dead monster can no longer attack)
+ *    				--made Hero and Monster abstract
+ *    				--created Warrior
+ *    				--created Ogre
+ *    				--made Warrior and Ogre battle
+ *    				--added battleChoices to Hero
+ *    				--added special skill to Warrior
+ *    				--made Warrior and Ogre battle
+ *    				--created Sorceress
+ *    				--created Thief
+ *    				--created Skeleton
+ *    				--created Gremlin
+ *    				--added game play features to Dungeon.java (this file)
+ *  			11/27/2001: Finished documenting program
+ * 				version 1.0
  */
 
 
